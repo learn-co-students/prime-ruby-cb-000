951d2ef57cd372b1db95de0cd11c46133e307463
Jordan Sleeper <ezirens@gmail.com>
2016-12-11 19:14:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  range = (2..num-1).to_a
+    m = range.any? do |x|
+    num % x == 0
+    #puts "#{num} % #{x} : #{num % x == 0}"
+    end
+  m ? false : true
+end
+
+puts Benchmark.measure{prime?(80000000)}
