463605603111cb82adbc88987d3c7c37bad4705e
Matthew <github email address>
2017-01-28 05:24:40 +0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(num)
+	if(num <= 1)
+		return false
+	end
+
+	factors = []
+
+	i = 2
+	while(i < num)
+		if(num % i == 0)
+			factors << i
+		end
+		i += 1
+	end
+
+	puts factors
+
+	if factors.length == 0
+		return true
+	else 
+		return false
+	end
+end
