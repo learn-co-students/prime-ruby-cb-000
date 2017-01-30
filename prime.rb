9476a0cc56a880bc3f0cb1a3c3739318ccbb6d4d
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-01-30 21:02:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+	return false if num < 2
+	index = 2
+	while index <= num/2
+		#puts "num: #{num}, index: #{index}, res:#{num % index}"
+		return false if num % index==0
+		index+=1
+	end
+	true
+end
