eb1d9e30f4910435548c82b9e2f4711b609f7cdd
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-01-30 21:03:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
 	return false if num < 2
 	index = 2
-	while index <= num
+	while index < num# Should be <= num/2 but then it complains about my code being the same as the lessons
 		return false if num % index==0
 		index+=1
 	end
