94498c6443b1ab1eaeead3dec33cc76cb10595b5
John Knotts <jhnnyk@gmail.com>
2017-01-31 16:11:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  # -1, 0 & 1 are not prime
+  if (-1..1).include?(number)
+    return false
+  end
+
+  # handle negative numbers
+  number = number.abs
+
+  upper = Math.sqrt(number).floor
+
+  potential_factors = Array (2..upper)
+
+  has_factors = potential_factors.map { |n| number % n == 0 }
+
+  has_factors.size == 0 || has_factors.all? { |factor| factor == false }
+end
