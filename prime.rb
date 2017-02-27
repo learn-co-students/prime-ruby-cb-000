36283f0454f1bc41120cdf12e3fd1f8fdb4ff79c
Kevin Tran <kevindutran@gmail.com>
2017-02-27 21:40:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,10 +3,10 @@ require 'pry'
 def prime?(n)
   if  n <= 1 ||(2**(n-1)) % n == 0
     return false
-  elsif n == 2 || n==3
+  elsif n == 2 
     return true
   else
-    range = [*3..n]
+    range = [*2..n]
     for num in range
       if num**(n-1) % n == 1
         return true
