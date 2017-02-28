b9d3b1be11dea0de63f6317ecea54d72c1e1329c
James Wilhelm <hello@jamesaverywilhelm.com>
2017-02-28 18:57:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
+#require 'benchmark'
 # Add  code here!
+
+def prime?(number)
+  return false if number < 2
+  range = (2..number - 1).to_a
+  range.each do |n|
+    return false if number % n == 0
+  end
+  return true
+end
