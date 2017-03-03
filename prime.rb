973b436572e5b89a1d262bfe2469832918fe1a42
Brian Johnson <overclocker303@gmail.com>
2017-03-03 19:45:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    false
+  elsif number >= 2 && number < 4
+    true
+  else
+    x = true
+    counter = 2
+    while counter < number
+      if number % counter == 0
+        x = false
+        break
+      else
+      end
+    counter += 1  
+    end
+    return x
+  end
+end
