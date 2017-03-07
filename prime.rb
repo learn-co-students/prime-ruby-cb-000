5352f658c1ff84b75f615188ff44a3738e901e5a
Andrew Zheng <andrew.zheng725@gmail.com>
2017-03-07 21:13:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  return false if number.even? or number <= 1
+  i = 3
+  while i*i < number
+    return false if number % i == 0
+    i += 2
+  end
+  return true
+end
