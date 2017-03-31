9fce64ee015eddb3218791d3ca533c7f64b176bb
Quinton Boltin <quinton@valleyexpetec.com>
2017-03-31 03:59:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(int)
+  if int == 0 || int.abs == 1 || int.abs % 2 == 0
+    false
+  elsif (3..(int.abs - 1)).to_a.none? {|i| (int.to_r/i).abs % 1 == 0}
+    true
+  else
+    false
+  end
+end
