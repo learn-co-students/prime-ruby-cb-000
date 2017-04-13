7deffac325f7e91fe340b2852bc7f779fac19034
Dan M <arconk@gmail.com>
2017-04-13 21:33:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(primo)
+  if primo <= 1
+    return false
+  elsif primo<=3
+    return true
+  elsif primo%2==0 || primo%3==0
+    return false
+  end
+  i = 5
+  while i*i<primo
+    if primo % i ==0 || primo %(i+2)==0
+      return false
+    end
+      i+=6
+      puts(i)if(i==5)
+      puts(i)if(i==305)
+  end
+  true
+end
+
+
+prime?(1763)
