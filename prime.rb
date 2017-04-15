f70e767348fc0dc8db03f6cdd79829d0d9eae5ca
nicolasqueen <nicolasqueen@gmail.com>
2017-04-15 14:24:10 -0400

    prime-ruby-cb-000

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+    i = 2
+    if number <= 1
+        return false
+    end
+    while i < number
+        if number % i == 0
+            return false
+        end
+        i += 1
+    end
+    true
+end
