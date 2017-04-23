569f4df44e7c50d69aed377098f07830e5f4d9ec
Alyssa Hogan <alyssabhogan@gmail.com>
2017-04-23 13:16:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,5 @@
 # Add  code here!
+def prime?(number)
+  # Check if number is greater than one and not divisible by any number between 2 and itself
+  number > 1 && (2..number-1).to_a.all?{ |n| number%n != 0 }
+end
