c9c4c19e8e6ed32033d32a7defcd342c2cb09011
Devan Moylan <dmmoylan@gmail.com>
2017-05-05 16:00:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+require 'pry'
+
+
+def prime?(n)
+  # check if even or less than 1
+  return false if n % 2 == 0 || n <= 1
+  return true if n == 2 || n == 3
+  i = 3
+  while i < n do
+    return false if n % i == 0
+    i += 1
+  end
+  true
+end
