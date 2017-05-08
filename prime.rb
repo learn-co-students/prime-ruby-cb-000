4fc50b8432b7013f3bd77bb7e52d734c88e8a507
Jorge Cossi <jorge_cossi@yahoo.com>
2017-05-08 15:29:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(a)
+  i = 2
+
+  if a > 1 
+    while i < a
+      if a % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+  else
+    return false
+  end
+    
+end
