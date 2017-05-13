1a66b56a1727cb0f6877e25265cda2cbd900e626
Chris Reed <cjr6439@gmail.com>
2017-05-12 20:51:36 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <= 2
+    return false
+  elsif num.even?
+    return false
+  end
+
+  max = (num**0.5).to_int #max would be square root of num
+
+  (3..max).each do |x|
+    if num % x == 0
+      return false
+    end
+  end
+  true
+end
