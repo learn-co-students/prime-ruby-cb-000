4248e8c0eaeb85c7142635aaea8b76fd326a82d2
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-17 13:47:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+require 'benchmark'
+
+def prime? number
+  return false if number < 2
+  (2..number/2).to_a.each do |devisible_by|
+    return false if number%devisible_by == 0
+  end
+  true
+end
+
+Benchmark.bm(15) do |bm|
+  bm.report("small number") do
+    10.times do
+      "10 #{prime?(10001)}" 
+    end
+  end
+  bm.report("big number") do
+    10.times do
+      "10 #{prime?(100001)}" 
+    end
+  end
+end
+ 
