17852ca063fea14c03c1e0d5907fd0077df815c4
DV <github email address>
2017-05-25 11:37:40 +0900

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+  return false if integer < 2
+  square_root = Math.sqrt(integer)
+  i = 2
+  while i <= (square_root)
+    return false if integer % i == 0
+    i += 1
+  end
+  return true
+end
