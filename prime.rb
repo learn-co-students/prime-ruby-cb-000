8f640f307304a16536329f2ac2badb3db2369a0a
Marcus Havanek <marcus.havranek@havranek.co.at>
2017-05-27 11:37:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(number)
+  return false if number <= 1 
+  limit = number / 2
+  results = []
+  for i in (2..limit) do
+    #puts "Number: #{number}, Limit: #{limit}, i: #{i}"
+    if number % i == 0
+      results << true
+    else
+      results << false
+    end
+  end
+
+  #puts "#{results}"
+  if results.include?(true)
+    return false
+  else
+    return true
+  end
+end
+
+#puts prime?(13)
