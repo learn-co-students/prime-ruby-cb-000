b626bdcc4f54648ce6e5b17a62e912a115c953ae
Tony Nassif <tonynassif@gmail.com>
2017-05-29 20:20:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(integer)
   i = 2
   return false if integer <= 1
@@ -7,6 +8,17 @@ def prime?(integer)
       break
     end
     i += 1
+  end
+  return true
+end
+=end
+
+def prime?(integer)
+  return false if integer <= 1
+  (2..integer-1).each do |num|
+    if integer % num === 0
+      return false
+    end
   end
   return true
 end
