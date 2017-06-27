2afea1a668e731622584b6fed9e9ea636f524283
william hancharek <william.hancharek@gmail.com>
2017-06-27 04:31:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'pry'
+
+def prime?(int)
+  array = (1.. 100).to_a
+
+  if int <= 0
+    return false
+  end
+
+  if int == 1
+    return false
+  end
+
+  array.map! {|div| int != div && int % div == 0 && div != 1 }
+  if array.include?(true)
+    return false
+  else
+    return true
+  end
+end
