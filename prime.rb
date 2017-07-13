d37d55e9356a73f10ee0c417ec4edf700b82d720
Ben Folk-Sullivan <benfolksullivan@gmail.com>
2017-07-13 02:07:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num < 2        #two is the first prime
+    return false
+  end
+  for i in 2..(num - 1)
+    if (num % i) == 0     #if num is divisible, it's not prime
+      return false
+    end
+  end
+  true
+end
