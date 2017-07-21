cd59f0796fe4fe1d00b297c2d7f29c1916e608cb
nikifi <nikifiona@gmail.com>
2017-07-20 20:29:05 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
+
+
+def prime?(num)
+	
+   counter = 2
+
+	if num <= 1
+      return false
+    end
+   
+while counter < num 
+
+	if (num % counter) == 0
+     return false
+
+	end
+
+	counter = counter + 1
+
+end
+
+return true
+
+end
 # Add  code here!
+
+puts prime?(8)
+
