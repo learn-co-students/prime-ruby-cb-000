958fc91688941edde9c97b4645860abd477b41da
Lashaunna Samuels <shaunna191@hotmail.com>
2017-08-03 19:45:50 -0400

    Prime number verifier

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Add  code here!
+def prime?(num)
+  # return false if any numbers are negative
+  return false if num < 2 
+  # create a range from 2 to num, if num is modulo any number in that range to equal 0 return false, if not return true.
+  if (2...num).any? { |x| num % x == 0 }
+    false
+  else
+    true
+  end
+end
