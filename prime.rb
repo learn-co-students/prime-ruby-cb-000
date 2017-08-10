5f72108fc083ab52a2b0f3dc363695a8fc3b4a60
Michael Palmer <lawlux@gmail.com>
2017-08-10 18:19:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(input)
+
+    if input <= 1
+        false
+    elsif input == 2
+        true
+    else
+        (2..input/2).none? { |i| input % i == 0}
+    end
+
+
+end
