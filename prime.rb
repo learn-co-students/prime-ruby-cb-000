1b447cf688982641cc132853e89c92e999648e1d
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-08-16 23:03:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def  prime?(value)
+  if value <= 1
+    return false
+  elsif value <= 3
+    return true
+  elsif value % 2 == 0 || value % 3 == 0
+    return false
+  end
+  i = 5
+  while i * i <= value
+    if value % i == 0 || value % (i + 2) == 0
+      return false
+    end
+    i = i + 6
+  end
+  return true
+end
