a17ed6e595765ac0c597def01ad622d892eb3e70
Joe Corwin <joe.corwin@gmail.com>
2017-08-22 14:03:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2
+  return true if number == 3 || number == 2
+    if (2...number-1).any? { |e| number % e == 0 }
+      false
+    else
+      true
+    end
+end
