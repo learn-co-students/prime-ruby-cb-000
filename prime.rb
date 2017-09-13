6cce91eab1eced254786f8e89e443ee865081ed3
Guilherme Sonoda <guilhermesnd@gmail.com>
2017-09-13 23:41:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  array = (2..(number-1)).to_a
+  if number <= 1 || array.any? { |e| number % e == 0 }
+    false
+  else
+    true
+  end
+  # for x in 2..(number-1)
+  #   if (number % x) == 0
+  #     false
+  #   end
+  # end
+  # true
+end
