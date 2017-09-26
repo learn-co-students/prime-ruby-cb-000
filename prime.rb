0bd86785b152e8aa76a6683b0c3c5266ccd7d982
Michael Anderson <michanders@msn.com>
2017-09-26 23:42:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(integer)
+  mike = (2..1_000_000).to_a
+  mike.each do |number|
+    if integer <= 1
+      return false
+    end
+    if integer == number
+      return true
+    elsif integer != number && integer % number == 0
+      return false
+    end
+  end
+end
