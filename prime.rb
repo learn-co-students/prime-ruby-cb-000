bd76ab44898ba811f96f9d4c20ef93d6169b7f86
Chris Cardle <cdcardle@gmail.com>
2017-10-30 22:35:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  if (n <= 1)
+    return false
+  elsif (n == 2 || n == 3)
+    return true
+  elsif (n % 2 == 0 || n % 3 == 0)
+    return false
+  else
+    (2..(n - 1)).each do |i|
+      if n % i == 0
+        return false
+      else
+        return true
+      end
+    end
+  end
+end
