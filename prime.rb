41516a10b4154f2c8886592a28e5cb1313bbf643
Crista Cooper <aine3d@gmail.com>
2017-11-20 02:47:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+
+
+def prime?(integer)
+
+  is_prime = true
+  return false if integer <= 1
+
+  for i in 2..integer - 1
+    if integer % i == 0 then
+      return false
+    end
+  end
+  return integer>1
+end
