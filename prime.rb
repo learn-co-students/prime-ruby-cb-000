5f4bb9fe40fc3c5a55f4313809082f390cd4ba79
bennybop <bennybop74@yahoo.com>
2017-11-30 01:49:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  x = (2..num)
+  if num <= 0 || num == 1
+    return false
+  elsif x.any? { |i| num > i && num % i == 0}
+    return false
+  else
+    return true
+  end
+end
