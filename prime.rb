102fbd38d47f3840608fae3944f693ab92f045b1
Kathy Hudson <khitx@icloud.com>
2018-01-14 20:56:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(int)
+  i = 2
+  if int < 2
+    return false
+  end
+  if int == 2
+    return true
+  end
+
+  while i <= int/2
+    if int % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
