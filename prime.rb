86a817065b307e9e2fefa074f9bc3c5396688545
Alyssa Foxx <alyssamariefoxx@gmail.com>
2018-01-30 16:31:01 -0500

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,8 +1,4 @@
 # Add  code here!
 def prime? n
-  if n > 0 &&  n != 1 && (2..Math.sqrt(n)).none? {|f| n % f == 0}
-    true
-  else
-    false
-  end
+  n > 0 &&  n != 1 && (2..Math.sqrt(n)).none? {|f| n % f == 0}
 end
