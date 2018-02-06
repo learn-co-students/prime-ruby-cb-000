b9ef5b07498b17206016c10ab0010a97aba7c7da
Edwin Padron <dwnpdrn@gmail.com>
2018-02-06 04:42:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime? number
+  if number < 2
+    return false
+  else
+    for d in 2..(number - 1)
+     if (number % d) == 0
+      return false
+     end
+    end
+  end
+  true
+end
