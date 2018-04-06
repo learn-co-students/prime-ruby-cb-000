234677919e99544f234f0d928bab144ad5214cfe
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-06 17:30:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+
+def prime?(n)
+  return false if n < 2
+  primes = []
+  sum = 0
+  for d in 2..(n-1)
+    sum += (d-(n%d))/d
+  end
+  (n**2-sum)/n**2 == 1 ? true : false
+end
