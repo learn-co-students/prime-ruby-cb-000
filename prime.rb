f9e545c952a0916b61d5fb96b369515ca7852600
Marcus Jun <mjun@calstate.edu>
2018-04-07 02:36:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 # Add  code here!
 
 def prime?(integer)
@@ -6,9 +7,29 @@ def prime?(integer)
   elsif integer == 2
     true
   else
-    num_array=(2..integer-1).to_a
+
+    #SLOW METHOD because array is larger [2..integer-1]
+    #num_array=(2..integer-1).to_a
+    #Benchmark.measure{prime?(105557)}
+    #0.014963-0.015795 seconds (ave 0.015379)
+
+    #FAST METHOD based on wikipedia "trial division" computational method
+    #num_array=(2..Math.sqrt(integer)).to_a
+    #divides integer from 2 - square root of integer
+    #Benchmark.measure{prime?(105557)}
+    #0.000100-0.000091 seconds (ave 0.0000955)
+    #which is 0.62% of the time it takes the slow method
+    #or 99.4% faster? My math is probably wrong.
+
+    #Both methods seem fast because they're measured in split seconds
+    #but I'm sure time savings add up if method is repeated many times
+    #or dealing with large numbers
+
+    num_array=(2..Math.sqrt(integer)).to_a
     num_array.all? do |number|
       integer % number != 0
     end
   end
 end
+
+#puts Benchmark.measure{prime?(105557)}
