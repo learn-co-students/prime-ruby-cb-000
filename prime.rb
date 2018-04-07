61c09c1a41905640800e50c8b865a421a5c5f08e
Marcus Jun <mjun@calstate.edu>
2018-04-07 01:18:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(integer)
+  if integer <= 1
+    false
+  elsif integer == 2
+    true
+  else
+    num_array=(2..integer-1).to_a
+    num_array.all? do |number|
+      integer % number != 0
+    end
+  end
+end
