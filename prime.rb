26ff5f3c7411882a2574c89d22d46cb0e2fda9ef
Sean Altemose <seanaltemose@icloud.com>
2018-04-11 17:47:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(int)
+  if int == 0 || int == 1
+    return false
+  elsif int < 0
+    return false
+  elsif
+  array = (2..int-1).to_a
+    if array.any? { |num| int % num == 0 }
+      return false
+    else true
+    end
+  end
+end
