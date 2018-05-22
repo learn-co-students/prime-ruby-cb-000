79b43d63a00b05b379f1b8f69cab21ed082bc5f3
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-05-22 11:30:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,49 +1,8 @@
-require 'benchmark'
-
-iterations = 1
-
-
 def prime?(number)
-  # make list of potential divisors
-  potentials = (2..(number/2).round).to_a
-  while potentials.length > 0
-    potentials.each do |pot|
-      if number % pot == 0
-        return false
-      else
-        potentials.each do |i|
-          if i % pot == 0
-            potentials.delete(i)
-          end
-        end
-      end
-    end
+  if number < 2
+    return false
+  else
+    (2..(number/2).round).each { |i|  return false if number % i == 0 }
   end
   return true
-end
-
-
-def prime2?(number)
-  (2..(number/2).round).each { |i|  return false if number % i == 0 }
-  return true
-end
-
-
-
-
-
-Benchmark.bm do |bm|
-  # joining an array of strings
-  bm.report do
-    iterations.times do
-      prime?(1221913)
-    end
-  end
-
-  # using string interpolation
-  bm.report do
-    iterations.times do
-      prime2?(1221913)
-    end
-  end
 end
