34a038e56d8af9dce6fa449fb4d956043f3fb20c
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-24 02:36:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+#def prime?(number)
+#  return false if number < 2
+#  p = 2
+#  while p < (2..number - 1).to_a
+#    number % i == 0 ? true : false
+#    p += 1
+#  end
+#end
+
+
+
+
+def prime?(number)
+  return false if number < 2
+  (2..number - 1).to_a.each do |i|
+   return false if number % i == 0
+  end
+  return true
+end
