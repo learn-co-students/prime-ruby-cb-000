cfbfe88714a58107080db84682f8cc15c75270ac
derrick b <dbozich1@gmail.com>
2018-05-25 23:39:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+
+
+def prime?(num)
+
+
+  possible_factors = (2..num).to_a
+  results = []
+
+  possible_factors.each do |i|
+    if num % i == 0
+      results.push(i)
+    end
+  end
+
+  results.first == num ? true : false
+end
