dc3471fe8cf4bb14d1070764590bbe5be0e8651d
Adam Urbina <adamurbina@gmail.com>
2018-06-01 01:12:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(number)
+
+    return false if number < 2
+
+    range = number
+    div = 2
+    while div < range
+        if number % div == 0
+            return false
+        else
+            range = (number/div).round
+            div += 1
+        end
+    end
+    return true
+end
