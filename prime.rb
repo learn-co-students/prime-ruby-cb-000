7a9c69babb89f393797afadf8c178881ab294014
Chris Franklin <cf73021@gmail.com>
2018-07-20 17:27:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  else
+    (2..number-1).to_a.all? do |i|
+      number % i != 0
+    end
+  end
+end
