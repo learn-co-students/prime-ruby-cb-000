12fb4cd7227dd89e0650d06b75d971f1a9c79e1b
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-10-01 21:29:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
 # Add  code here!
+#def prime?(n)
+#  counter = 0
+#  if n <= 0 || n == 1
+#    return false
+#  elsif (n % 1 == 0) && (n % n == 0)
+#    return true
+#    numbers = (2..10000).to_a
+#    numbers.map do |num|
+#      if n % num == 0
+#        return false
+#      end
+#    end
+#  end
+#end
+
+def prime?(n)
+  if n < 2
+    return false
+  else
+    numbers = (2..(n-1)).to_a
+    numbers.map do |num|
+      if n % num == 0
+        return false
+      end
+    end
+  return true
+  end
+end
