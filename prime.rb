abee322562d78929311a1a312301f84c753ea4b4
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-26 05:33:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
+require 'benchmark'
+
+
+# calculate pi to 10k digits
+
+
+puts Benchmark.measure {
+  100_000.times do
 # Add  code here!
 def prime?(n)
   if n < 0 || n == 0 || n == 1
@@ -7,4 +15,6 @@ def prime?(n)
       n%num != 0
     end
   end
-end 
+end
+end
+}
