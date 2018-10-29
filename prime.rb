76c86a9de18b5cd54444d32790b36389db61adcd
Fer Hernandez <fh.1111@gmail.com>
2018-10-29 01:13:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,32 @@
 # Add  code here!
+
+def prime? (number)
+
+  if (number > 1 && number <= 2)
+    return true
+
+  elsif number > 2
+
+    count = 0
+    i = 2
+      while (i<number)
+
+        if  (number % i == 0)
+          count += 1
+        end
+        i+=1
+      end
+
+      if (count == 0)
+        return true
+      else
+        return false
+      end
+
+  else
+    return false
+  end
+
+end
+
+prime?(3)
