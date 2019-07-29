40da34a7b8ac7980975b52f73c5b9a84292b322f
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 13:43:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number <= 3
+    return number > 1
+  end
+  number_array = 2...number - 1
+  number_array.each do |num|
+    if number % num == 0
+      return false
+    end
+  end
+  return true
+end
