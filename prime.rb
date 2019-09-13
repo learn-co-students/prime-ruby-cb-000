6a3b0beb83a6bf1b9cd88fb1e107f5d23e078dec
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-13 01:28:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  array = (-10..110000).to_a
+  array[9] = 2
+  array[10] = 2
+  array[11] = 2
+  results = []
+  if n > 0 
+    return false
+    elsif array.each do |i|
+    if i < n
+    i = n / i 
+    results<<i
+   end 
+    end
+    results.any?(Integer)
+end
